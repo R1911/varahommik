4afa6e66641aeafac32ab650794a67c80de9a3d4
Riivo <riivopikson@gmail.com>
2024-10-19 15:05:29 +0300

    Updates

--- src/cronJobs.js
+++ src/cronJobs.js
@@ -1,11 +1,12 @@
 const { CronJob } = require('cron');
 const { updateImages } = require('./tasks/updateImages');
-const { tweetWeatherUpdate } = require('./tasks/tweetWeather');
+const { tweetWeatherUpdate, checkEarlyWeather } = require('./tasks/tweetWeather');
 const { tweet } = require('./tasks/tweetContent');
 
 const scheduleCronJobs = () => {
     new CronJob("0 0 0 1 * *", updateImages, null, true, 'Europe/Tallinn');
     new CronJob("0 0 8 * * *", tweetWeatherUpdate, null, true, 'Europe/Tallinn');
+    new CronJob("0 0 7 * * *", checkEarlyWeather, null, true, 'Europe/Tallinn', true);
     new CronJob("0 30 5 * * *", tweet, null, true, 'Europe/Tallinn');
 };
 
--- src/tasks/tweetWeather.js
+++ src/tasks/tweetWeather.js
@@ -6,29 +6,53 @@ const { getDateTime } = require('../utils/dateTimeHelper');
 
 // Function to prepare and tweet weather update
 async function tweetWeatherUpdate() {
-    const dateTime = getDateTime();
-    try {
-        const weather = await scrapeWeatherDetails();
-        const { phenomenon, tempmin, tempmax } = weather;
-        let weatherContent;
+    const dateTime = new Date();
+    const currentDate = dateTime.getDate();
+    const month = dateTime.getMonth() + 1;
+    let earlyWeather;
 
+    if (month >= 9 || month <= 4) {
+        const weatherDataRaw = fs.readFileSync(path.join(__dirname, "../weatherData.json"));
+        earlyWeather = JSON.parse(weatherDataRaw);
+    }
+
+    const weather = await scrapeWeatherDetails();
+    const { phenomenon, tempmin, tempmax } = weather;
+    let weatherContent;
+
+    if (earlyWeather && currentDate === earlyWeather.dateChecked) {
+        const adjustedTempMin = Math.min(tempmin, earlyWeather.tempmin);
+        weatherContent = `Tänane õhutemperatuur jääb vahemikku ${adjustedTempMin} kuni ${tempmax} kraadi. ${phenomenon}`;
+    } else {
         if (tempmin > 15) {
             weatherContent = `Täna on sooja kuni ${tempmax} kraadi. ${phenomenon}`;
         } else {
             weatherContent = `Tänane õhutemperatuur jääb vahemikku ${tempmin} kuni ${tempmax} kraadi. ${phenomenon}`;
         }
+    }
 
-        // Read the last tweet ID to reply to
-        const tweetDataPath = path.join(__dirname, "../tweet.json");
-        const tweetDataRaw = fs.readFileSync(tweetDataPath);
-        const { tweetId } = JSON.parse(tweetDataRaw);
+    const tweetDataPath = path.join(__dirname, "../tweet.json");
+    const tweetDataRaw = fs.readFileSync(tweetDataPath);
+    const { tweetId } = JSON.parse(tweetDataRaw);
 
-        // Reply to the previous tweet with weather update
-        const response = await twitterClient.v2.reply(weatherContent, tweetId);
-        console.info(`${getDateTime()} Replied to tweet ID: ${tweetId} with weather update.`);
-    } catch (error) {
-        console.error(`${getDateTime()} Failed to tweet weather update:`, error);
+    const response = await twitterClient.v2.reply(weatherContent, tweetId);
+    console.info(`${getDateTime()} Replied to tweet ID: ${tweetId} with weather update.`);
+}
+
+
+async function checkEarlyWeather() {
+    const currentDate = new Date();
+    const month = currentDate.getMonth() + 1;
+    if (month >= 9 || month <= 4) {  // Check weather from September to April
+        const weather = await scrapeWeatherDetails();
+        const weatherData = {
+            tempmin: weather.tempmin,
+            dateChecked: currentDate.getDate()
+        };
+        fs.writeFileSync(path.join(__dirname, "../weatherData.json"), JSON.stringify(weatherData));
+        console.log(`${getDateTime()} Stored early morning weather data`);
     }
 }
 
-module.exports = { tweetWeatherUpdate };
+
+module.exports = { tweetWeatherUpdate, checkEarlyWeather };
